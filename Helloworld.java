98bafd8d06eb64ae122c55120d41561f182b65c7
viswa <viswa.dm@gmail.com>
2017-12-14 21:31:39 +0530

    kittu

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main( String[] args ) {
-        System.out.println( "Hello viswa!" );
+        System.out.println( "Hello kittu!" );
         System.exit( 0 ); //success
     }
 }
